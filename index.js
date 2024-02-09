e9d0aa947b6cdbd647c128771e8eeac629c8dbf6
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-02-09 16:28:12 +0000

    added

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.initialize();
